f2c3824f409bc84079f0d55c3aa23245d166b7fc
Ivan Baldinotti <ivan.baldinotti@gmail.com>
2017-10-23 11:54:46 +0200

    moved packages to the LFS env

--- get_packages.sh
+++ get_packages.sh
@@ -2,6 +2,12 @@
 
 echo "Installing the packages and the patches"
 
-wget --input-file=./wget-files/packages-list --continue --directory-prefix=./packages
-wget --input-file=./wget-files/packages-patches-list --continue --directory-prefix=./packages/patches
+mkdir $LFS/tools/packages
+mkdir $LFS/tools/packages/patches
+
+chmod 777 $LFS/tools/packages
+chmod 777 $LFS/tools/packages/patches
+
+wget --input-file=./wget-files/packages-list --continue --directory-prefix=$LFS/tools/packages
+wget --input-file=./wget-files/packages-patches-list --continue --directory-prefix=$LFS/tools/packages/patches
 
